37eca0fdab557fec62e46ca432968abc649cb887
natalia <natakarelskaya@yandex.ru>
2024-04-15 09:18:38 +0400

    переключение табов ok

--- tabs.js
+++ tabs.js
@@ -1,29 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  const tabItems = document.querySelectorAll('.tab-button');
-  console.log(tabItems);
+  const tabs = document.querySelectorAll('.tab-button');
+  const tabContents = document.querySelectorAll('.tab-content');
 
-  tabItems.forEach(function(tab) {
-    // console.log(tab);
-    tab.addEventListener('click', function() {
-      const tabId = this.dataset.tab;
-      console.log(tabId);
-      const tabContent = document.querySelector(`.tab-content[id="${tabId}"]`);
-      console.log(tabContent);
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () =>{
+      
+      tabs.forEach(tab => tab.classList.remove('active'));
+      tab.classList.add('active');
 
-      // tabItems.forEach(function(tab) {
-      //   tab.classList.remove('active');
-      // });
-
-      tabItems.forEach(tab => {
-        console.log(tab);
-        tab.classList.remove('active');
-      });
-
-      this.classList.add('active');
-
-      tabContent.classList.add('active');
-      tabContent.classList.remove('hide');
+      tabContents.forEach(content => content.classList.remove('active'));
+    
+      const target = document.getElementById(tab.dataset.tab);
+      target.classList.add('active');
     });
   });
 });
